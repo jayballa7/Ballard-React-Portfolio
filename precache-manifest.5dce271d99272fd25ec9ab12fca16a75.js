df41ab5dfe321d66ac14ee784506021f301cbdb8
Jennifer Ballard <jenn_ballard7@hotmail.com>
2020-06-03 20:34:24 -0700

    Updates

---
+++ precache-manifest.5dce271d99272fd25ec9ab12fca16a75.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38fd1d0c6d920bf6024cc7bad26200d",
+    "url": "/Ballard-React-Portfolio/index.html"
+  },
+  {
+    "revision": "879e5f10f562d6f26fa3",
+    "url": "/Ballard-React-Portfolio/static/css/main.d21d4aa8.chunk.css"
+  },
+  {
+    "revision": "446172393db61ab2b76c",
+    "url": "/Ballard-React-Portfolio/static/js/2.bee8f65c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Ballard-React-Portfolio/static/js/2.bee8f65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879e5f10f562d6f26fa3",
+    "url": "/Ballard-React-Portfolio/static/js/main.236a9396.chunk.js"
+  },
+  {
+    "revision": "8abed7e04f1452eba706",
+    "url": "/Ballard-React-Portfolio/static/js/runtime-main.872c0f1d.js"
+  },
+  {
+    "revision": "c8e0dbe55f1f90026171727c5e756954",
+    "url": "/Ballard-React-Portfolio/static/media/astronaut.c8e0dbe5.jpg_fit=scale"
+  },
+  {
+    "revision": "9f3c5b80a2253bcf6393143f83df01a5",
+    "url": "/Ballard-React-Portfolio/static/media/crimeradar.9f3c5b80.PNG"
+  },
+  {
+    "revision": "45a95aa4ffa2280dce6fc3186a91cb34",
+    "url": "/Ballard-React-Portfolio/static/media/eatdaburger.45a95aa4.png"
+  },
+  {
+    "revision": "dea38a93417f6c86ef04c10275108862",
+    "url": "/Ballard-React-Portfolio/static/media/employeetracker.dea38a93.png"
+  },
+  {
+    "revision": "8daad60833801640685e57e05d4e3fc9",
+    "url": "/Ballard-React-Portfolio/static/media/newsflash.8daad608.PNG"
+  },
+  {
+    "revision": "5b425f56d74b1d9e7fa5a2ff6a54aed3",
+    "url": "/Ballard-React-Portfolio/static/media/profile-photo.5b425f56.jpg"
+  },
+  {
+    "revision": "1c374c4d0114b5bc33716f03a368fcc9",
+    "url": "/Ballard-React-Portfolio/static/media/rendezvous.1c374c4d.PNG"
+  },
+  {
+    "revision": "de2225ff974479a084d64d905dc1172a",
+    "url": "/Ballard-React-Portfolio/static/media/star-background.de2225ff.jpg"
+  },
+  {
+    "revision": "fb71a5cd1bc74abf1b4ae2c46b3069cb",
+    "url": "/Ballard-React-Portfolio/static/media/weatherdashboard.fb71a5cd.PNG"
+  }
+]);
