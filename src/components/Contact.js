48cbba66444d9742149e874815cc22ba79fd7bd4
Jenn Ballard <jenn_ballard7@hotmail.com>
2020-09-24 18:50:34 -0700

    fixed responsiveness of contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -32,15 +32,6 @@ function Contact() {
 
 
 </div>
-{/* 
-                <div className = "contact-card">
-                    <h2>Contact Information</h2>
-                        <div className="separator"></div> 
-                    <h3>Jennifer Ballard</h3>
-                    <p>Phone Number: 425-443-2792</p>
-                    <p>Email: jenn_ballard7@hotmail.com</p>
-                    <p>Located in Renton, Washington</p>
-                </div> */}
 
             </div>
 
